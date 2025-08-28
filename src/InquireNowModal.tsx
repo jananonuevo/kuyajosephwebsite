e917146fe4b67ec87234741a2a10faedf25669f0
jananonuevo <jananonuevo7@gmail.com>
2025-08-28 19:00:06 +0800

    inquiry form

--- src/InquireNowModal.tsx
+++ src/InquireNowModal.tsx
@@ -13,6 +13,7 @@ const InquireNowModal: React.FC<InquireNowModalProps> = ({ isOpen, closeModal })
     name: "",
     email: "",
     contactNo: "",
+    inquiry: ""
   });
 
   // Reference to the Firestore collection
@@ -37,7 +38,7 @@ const InquireNowModal: React.FC<InquireNowModalProps> = ({ isOpen, closeModal })
       closeModal(); // Close the modal on success
       
       // Reset the form state
-      setFormData({ name: "", email: "", contactNo: "" });
+      setFormData({ name: "", email: "", contactNo: "", inquiry: "" });
 
     } catch (error) {
       console.error("Error adding document: ", error);
@@ -104,6 +105,17 @@ const InquireNowModal: React.FC<InquireNowModalProps> = ({ isOpen, closeModal })
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
               />
               <label htmlFor="contactNo" className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-autopeer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Contact Number</label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input 
+                type="text" 
+                name="inquiry" 
+                id="inquiry" 
+                value={formData.inquiry}
+                onChange={handleChange}
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
+              />
+              <label htmlFor="email" className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-autopeer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Inquiry</label>
             </div>
             <center>
               <button className="cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-15 border border-gray-400 rounded shadow mt-5" type="submit">
--- src/Inquiries.tsx
+++ src/Inquiries.tsx
@@ -1,21 +1,68 @@
-import { Link } from "react-router-dom";
+import React from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "./firebaseConfig.ts";
 
 function Inquiries() {
 
+  const [inquiries, setInquiries] = React.useState<any[]>([]);
+  
   async function getCollections() {
+    const tempCollections = [];
     const collections = await getDocs(collection(firestore, "inquiries"));
-
+    
     for (let i = 0; i < collections.docs.length; i++) {
-      console.log("HIIIIII");
-      console.log(collections.docs[i].data());
+      tempCollections.push(collections.docs[i].data());
     }
+    setInquiries(tempCollections);
+  }
+
+  getCollections();
+
+  const rows = [];
+  for (let i = 0; i < inquiries.length; i++) {
+    rows.push(
+      <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          {inquiries[i].name}
+        </th>
+        <td className="px-6 py-4">
+          {inquiries[i].contactNo}
+        </td>
+        <td className="px-6 py-4">
+          {inquiries[i].email}
+        </td>
+        <td className="px-6 py-4">
+          {inquiries[i].inquiry}
+        </td>
+      </tr>
+    );
   }
   
   return (
     <>
-    fdsafd
+      <div className="mt-10 mx-10">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Contact No.
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Inquiry
+                </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows} 
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
