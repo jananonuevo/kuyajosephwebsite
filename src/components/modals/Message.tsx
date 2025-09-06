26c5b257d3fc19e2afbf4b368c40c06db1a8d896
jananonuevo <jananonuevo7@gmail.com>
2025-09-07 03:34:50 +0800

    small fixes, added delete button

--- src/components/modals/Message.tsx
+++ src/components/modals/Message.tsx
@@ -17,7 +17,7 @@ const Message: React.FC<MessageProps> = ({ isOpen, closeModal }) => {
       style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}
       className="fixed inset-0 z-50 flex items-center justify-center p-4 overflow-y-auto"
     >
-      <div className="relative w-full max-w-md p-6 mx-auto transition-all duration-100 ease-in-out transform scale-100 bg-white rounded-lg shadow-2xl">
+      <div className="relative w-full max-w-md p-6 mx-auto transition-all duration-100 ease-in-out transform scale-100 bg-white/95 rounded-lg shadow-2xl">
         
         <Header isOpen={isOpen} closeModal={closeModal} />
         <Content />
--- src/pages/Inquiries.tsx
+++ src/pages/Inquiries.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { firestore } from "../firebaseConfig.ts";
 import { Link } from "react-router-dom";
 
@@ -12,18 +12,34 @@ function Inquiries() {
     const collections = await getDocs(collection(firestore, "inquiries"));
     
     for (let i = 0; i < collections.docs.length; i++) {
-      tempCollections.push(collections.docs[i].data());
+      tempCollections.push({
+        id: collections.docs[i].id, 
+        ...collections.docs[i].data(),
+      });
     }
     setInquiries(tempCollections);
   }
+
+  const deleteDocumentById = async (collectionName: any, documentId: any) => {
+  try {
+    const docRef = doc(firestore, collectionName, documentId);
+    await deleteDoc(docRef);
+    console.log("Document with ID:", documentId, "deleted successfully!");
+  } catch (error) {
+    console.error("Error removing document: ", error);
+  }
+};
 
   getCollections();
 
   const rows = [];
   for (let i = 0; i < inquiries.length; i++) {
     rows.push(
-      <div className = "bg-amber-20 border-t-3 rounded-lg w-100 my-5 shadow-lg">
-        <h1 className = "text-md font-bold mx-4 mt-5">{inquiries[i].name}</h1>
+      <div className = "bg-amber-20 border-t-3 rounded-lg lg:w-9/10 my-5 shadow-lg">
+        <div className="grid grid-cols-2">
+          <h1 className = "text-md font-bold mx-4 mt-5">{inquiries[i].name}</h1>
+          <h1 className = "cursor-pointer text-sm mx-4 mt-5 font-bold text-right text-red-400" onClick={() => deleteDocumentById("inquiries", inquiries[i].id)}>X</h1>
+        </div>
         <p className = "text-sm mx-4 mt-1">{inquiries[i].contactNo} || {inquiries[i].email}</p>
         <hr className = "mt-2"></hr>
         <p className = "text-sm text-center mx-4 my-5">{inquiries[i].inquiry}</p>
@@ -40,14 +56,7 @@ function Inquiries() {
 
     <div className = "flex justify-center align-items-center ">
 
-      <div className="grid grid-cols-4 gap-5 mt-10">
-
-      <div className = "bg-amber-20 border-t-3 rounded-lg w-100 my-5 shadow-lg">
-        <h1 className = "text-md font-bold mx-4 mt-5">Jan Anonuevo</h1>
-        <p className = "text-sm mx-4 mt-1">0969 231 7498 || jananonuevo7@gmail.com</p>
-        <hr className = "mt-2"></hr>
-        <p className = "text-sm text-center mx-4 my-5">Hello, I would like to inquire about your services. Please let me know how I can get in touch with you. Thank you!</p>
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-5 mt-10">
       
       {rows}
 
